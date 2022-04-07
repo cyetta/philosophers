c0ff295e75f758f9a0e532bd534b3638fdf482c2
Cari Yetta <cyetta@student.msk.21-school.ru>
2022-04-07 22:22:13 +0300

    added function init_ph, not tested

--- philo/philo.h
+++ philo/philo.h
@@ -6,20 +6,33 @@
 /*   By: cyetta <cyetta@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 18:04:40 by cyetta            #+#    #+#             */
-/*   Updated: 2022/04/02 19:55:16 by cyetta           ###   ########.fr       */
+/*   Updated: 2022/04/07 22:22:13 by cyetta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+# include <pthread.h>
+# include <>
+
+typedef struct s_ph_param
+{
+	int			numb_philo;
+	int			time_to_die;
+	int			time_to_eat;
+	int			time_to_sleep;
+	int			numb_ph_eat;
+	int			*mtx_print;
+	int			*mx_forks;
+}	t_ph_param;
 
 typedef struct s_philo
 {
-	int	numb_philo;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	numb_ph_eat;
+	t_ph_param	*param;
+	int			ph_num;
+	long		last_eat_usec;
+	int			is_die;
+	pthread_t	ph_thread;
 }	t_philo;
 
 #endif
